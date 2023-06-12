6f7da7dc207425b869ca21d60935f7d44146a58a
Drew <37987027+agdeon@users.noreply.github.com>
2023-06-12 15:18:16 +0300

    v1

---
+++ script.js
@@ -0,0 +1,21 @@
+var styles = `
+   .btn_dwnl_mp3 {
+    display: inline-block;
+    background: rgb(8,69,144);
+    color: #fff;
+    padding: 0.2% 1%;
+    text-decoration: none;
+    border-radius: 5%;
+    position: relative;
+    left: 10px;
+   }
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
+phonsnamDiv = document.body.querySelector(".phons_n_am")
+mp3_url = phonsnamDiv.querySelector("div").getAttribute("data-src-mp3")
+url_splitted = mp3_url.split("/")
+file_name = url_splitted[url_splitted.length-1]
+phonsnamDiv.innerHTML = phonsnamDiv.innerHTML + `<a class="btn_dwnl_mp3" href="${mp3_url}" download="${file_name}">Download mp3</a>`
