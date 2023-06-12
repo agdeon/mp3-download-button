1ccb831c79d763953c73a0e796a2bdd84ce1e2a2
Drew <37987027+agdeon@users.noreply.github.com>
2023-06-12 15:56:56 +0300

    Update script.js
    фикс звука

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var styles = `
    .btn_dwnl_mp3 {
-    display: inline-block;
-    background: rgb(8,69,144);
-    color: #fff;
-    padding: 0.2% 1%;
-    text-decoration: none;
-    border-radius: 5%;
-    position: relative;
-    left: 10px;
+      display: inline-block;
+      background: rgb(8,69,144);
+      color: #fff;
+      padding: 0.2% 1%;
+      text-decoration: none;
+      border-radius: 5%;
+      position: relative;
+      left: 10px;
    }
 `
 
 var styleSheet = document.createElement("style")
 styleSheet.innerText = styles
 document.head.appendChild(styleSheet)
+
 phonsnamDiv = document.body.querySelector(".phons_n_am")
 mp3_url = phonsnamDiv.querySelector("div").getAttribute("data-src-mp3")
 url_splitted = mp3_url.split("/")
 file_name = url_splitted[url_splitted.length-1]
-phonsnamDiv.innerHTML = phonsnamDiv.innerHTML + `<a class="btn_dwnl_mp3" href="${mp3_url}" download="${file_name}">Download mp3</a>`
+
+// так не работает звук
+// phonsnamDiv.innerHTML = phonsnamDiv.innerHTML + `<a class="btn_dwnl_mp3" href="${mp3_url}" download="${file_name}">Download mp3</a>`
+
+dwnl_button = document.createElement("a")
+dwnl_button.innerText = "Download mp3"
+dwnl_button.classList.add("btn_dwnl_mp3")
+dwnl_button.setAttribute("href", mp3_url)
+dwnl_button.setAttribute("download", file_name)
+phonsnamDiv.appendChild(dwnl_button)
